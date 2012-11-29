30d7abc894228f8ba38f5a05cc1bf398f62bd04e
Kendell Joseph <contact@kendelljoseph.com>
2012-11-29 02:57:16 -0500

    Updated example to work, HTML still under construction though

--- example/javascript/example.js
+++ example/javascript/example.js
@@ -1,20 +1,47 @@
-var studentRecipes = getGSS;                                    // Rename getGSS (this is mainly for use in cloud 9)
-var GSS_KEY = "0AhepTSuBIkmLdFdrUDZnRDhnRmFtbTNCclhNcUVQRXc";   // Student Recipes GSS key
-var GSS_URL = "https://spreadsheets.google.com/feeds/list/"+    // Student Recipes GSS URL
-            GSS_KEY + "/od6/public/values?alt=json";            
-
-studentRecipes(GSS_URL, function(rows) {    // Pass the GSS URL
-    var row = rows[0];          // The GSS rows
-    var imageURL = row[3];      // The Recipe Image URL
-    var description = row[7];   // The Recipe Description
+$(function(){
+    var studentRecipes = getGSS;                                    // Rename getGSS (this is mainly for use in cloud 9)
+    var GSS_KEY = "0AhepTSuBIkmLdFdrUDZnRDhnRmFtbTNCclhNcUVQRXc";   // Student Recipes GSS key
+    var GSS_URL = "https://spreadsheets.google.com/feeds/list/"+    // Student Recipes GSS URL
+                GSS_KEY + "/od6/public/values?alt=json";            
     
-    var recipe = $('#recipe'); // The Recipe
-    var name = $('<header />')   // The Recipe Element
-            .html(row[6]);      // Insert the recipe name row
-    
-    recipe.append(name);
+    studentRecipes(GSS_URL, function(rows) {    // Pass the GSS URL
+        var nextRecipeButton = $('#nextRecipeButton');
+        var currentRecipe = $('#currentRecipe');
         
-    // Debug
-    console.log(recipe, description, imageURL);
-
+        function showRecipe(){
+            var columns = rows[currentRecipe.val()];
+            var recipe = $('#recipe');      // The Recipe Element
+            var timestamp = $('<span />')   // The Recipe Timestamp
+                    .attr("id", "timestamp")
+                    .html(columns[0]);
+            var ingredients = $('<span />') // The Ingredients
+                    .attr("id", "ingredients") 
+                    .html(columns[13]);     // GSS column n
+            var name = $('<header />')      // The Recipe Name
+                    .html(columns[6]);      // GSS Column F
+            var description = $('<p />')    // The Recipe Description
+                    .html(columns[7]);      // GSS Column G
+            var image = $('<img />')            // The Recipe Image
+                .attr("src", columns[3]);   // GSS Column D
+            recipe.html("");
+            recipe.append(timestamp);
+            recipe.append(ingredients);
+            recipe.append(name);
+            recipe.append(image);
+            recipe.append(description);
+                
+            // Debug
+            console.log(recipe, name, image);
+        }
+        
+        nextRecipeButton.mousedown(showRecipe);
+        currentRecipe.keypress(function(){
+            if ( event.which == 13 ) {
+                event.preventDefault();
+                showRecipe();
+           }
+        });
+        
+        showRecipe();
+    });
 });
