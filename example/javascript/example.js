c2bfdd11da76c7e46e24eca08233efb868c2f346
Kendell Joseph <contact@kendelljoseph.com>
2012-11-29 00:15:11 -0500

    Added html output to page

--- example/javascript/example.js
+++ example/javascript/example.js
@@ -3,14 +3,18 @@ var GSS_KEY = "0AhepTSuBIkmLdFdrUDZnRDhnRmFtbTNCclhNcUVQRXc";   // Student Recip
 var GSS_URL = "https://spreadsheets.google.com/feeds/list/"+    // Student Recipes GSS URL
             GSS_KEY + "/od6/public/values?alt=json";            
 
-
 studentRecipes(GSS_URL, function(rows) {    // Pass the GSS URL
     var row = rows[0];          // The GSS rows
     var imageURL = row[3];      // The Recipe Image URL
-    var recipieName = row[6];   // The Recipe Name
     var description = row[7];   // The Recipe Description
     
-    console.log(recipieName, description, imageURL);
-    console.log(row);
+    var recipe = $('#recipe'); // The Recipe
+    var name = $('<header />')   // The Recipe Element
+            .html(row[6]);      // Insert the recipe name row
     
+    recipe.append(name);
+        
+    // Debug
+    console.log(recipe, description, imageURL);
+
 });
