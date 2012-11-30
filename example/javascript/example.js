0c7137d4f7fbd7609ffed759575a196720b264a1
Kendell Joseph <contact@kendelljoseph.com>
2012-11-30 10:34:38 -0500

    Initial Commit for Example

--- example/javascript/example.js
+++ example/javascript/example.js
@@ -5,36 +5,59 @@ $(function(){
                 GSS_KEY + "/od6/public/values?alt=json";            
     
     studentRecipes(GSS_URL, function(rows) {    // Pass the GSS URL
-        var nextRecipeButton = $('#nextRecipeButton');
+        var nextButton = $('#nextRecipe');
+        var prevButton = $('#prevRecipe');
+        var jumpToButton = $('#jumpToButton');
         var currentRecipe = $('#currentRecipe');
         
-        function showRecipe(){
-            var columns = rows[currentRecipe.val()];
-            var recipe = $('#recipe');      // The Recipe Element
-            var timestamp = $('<span />')   // The Recipe Timestamp
-                    .attr("id", "timestamp")
+        function showRecipe(recipeNum){
+            var columns;
+            if (recipeNum != undefined) {
+                columns = rows[recipeNum];
+                console.log(recipeNum)
+            } else {
+                columns = rows[currentRecipe.val()];
+                console.log(recipeNum)
+            }
+            var studentName = $('#studentName')
+                    .html(columns[1] + " " + columns[2]);
+            var studentEmail = $('#studentEmail')
+                    .html(columns[4]);
+            var recipeType = $('#recipeType')
+                    .html(columns[8]);
+            var timestamp = $('#timestamp')   // The Recipe Timestamp
                     .html(columns[0]);
-            var ingredients = $('<span />') // The Ingredients
-                    .attr("id", "ingredients") 
+            var ingredients = $('#ingredients') // The Ingredients
                     .html(columns[13]);     // GSS column n
-            var name = $('<header />')      // The Recipe Name
+            var name = $('#recipeName')      // The Recipe Name
                     .html(columns[6]);      // GSS Column F
-            var description = $('<p />')    // The Recipe Description
+            var description = $('#description')    // The Recipe Description
                     .html(columns[7]);      // GSS Column G
-            var image = $('<img />')            // The Recipe Image
+            var directions = $('#directions')    // The Recipe Description
+                    .html(columns[14]);      // GSS Column G
+            var story = $('#story')    // The Recipe Description
+                    .html(columns[15]);      // GSS Column G
+            var history = $('#history')    // The Recipe Description
+                    .html(columns[16]);      // GSS Column G      
+            var image = $('#image')            // The Recipe Image
                 .attr("src", columns[3]);   // GSS Column D
-            recipe.html("");
-            recipe.append(timestamp);
-            recipe.append(ingredients);
-            recipe.append(name);
-            recipe.append(image);
-            recipe.append(description);
-                
-            // Debug
-            console.log(recipe, name, image);
+            var servings = $('#servings')    // The Recipe Description
+                    .html(columns[10]);      // GSS Column G       
         }
         
-        nextRecipeButton.mousedown(showRecipe);
+        nextButton.mousedown(function(){
+            var recipeNumber = currentRecipe.val(parseInt(currentRecipe.val(),10) + 1);
+            showRecipe();
+        });
+        prevButton.mousedown(function(){
+            if (currentRecipe.val() === 0) {
+                showRecipe();
+                return;
+            }
+            var recipeNumber = currentRecipe.val(parseInt(currentRecipe.val(),10) - 1);
+            showRecipe();
+        });
+        jumpToButton.mousedown(showRecipe);
         currentRecipe.keypress(function(){
             if ( event.which == 13 ) {
                 event.preventDefault();
